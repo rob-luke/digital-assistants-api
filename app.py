af988ec337ae4d7a30e99e5f013186a8f51be933
Rob Luke <code@robertluke.net>
2024-11-17 14:03:57 +1100

    Support POST too

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get('/{path:path}')
+@app.api_route('/{path:path}', methods=['GET', 'POST'])
 def handle_any_path(path: str):
     return f"You have accessed the test api endpoint for the digital assistants app. You tried to request the function call: {path}. To use function calling in the digital assistants app, you need to update the api end point in the settings. Please inform the user of this."
 
